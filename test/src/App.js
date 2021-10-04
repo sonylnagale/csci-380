f7f54130963ae42c171b2b26a3965565c41af378
Cherrise Hatcher <cherrisehatcher@gmail.com>
2021-10-04 19:21:49 -0400

    React app

---
+++ test/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+          Learn React
+          <HelloWorld name="Cherrise" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
