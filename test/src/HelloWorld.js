8ac277ff74812a07143cfebdbf23d44649b79f31
April_Hamilton_2022 <ahamiltonmaa@gmail.com>
2021-10-04 19:21:08 -0400

    10/4
    New Classwork

---
+++ test/src/HelloWorld.js
@@ -0,0 +1,23 @@
+import React,{useEffect, useState} from 'react';
+
+const HelloWorld = (props) => {
+    const[counter, setCounter] = useState(0);
+
+    const incrementCounter =() =>{
+        setCounter(counter + 1)
+    }
+
+
+    return(
+        <>
+            <>
+              <p>Hello</p>
+              <h1>{props.name}</h1>
+         </>
+        <dic><p>{counter}</p></dic>
+        <button onClick={incrementCounter}>Increment Me</button>
+        </>
+    )
+}
+
+export default HelloWorld;
