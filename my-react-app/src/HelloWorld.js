17611b99be9efabf57b613f2d1f23f9718597031
vincepogz <85046947+vincepogz@users.noreply.github.com>
2021-10-04 19:30:10 -0400

    Test

---
+++ my-react-app/src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HelloWorld = () => {
+    return(
+        <>
+        <div>
+            <p>Hello</p>
+            <h1>WORLD!</h1>
+        </div>
+        <div><p>Hello</p></div>
+        </>
+    )
+}
+export default HelloWorld;
