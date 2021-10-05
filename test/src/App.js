a8730d5c9b70ba9814a41093e40f7030a385257a
SumaiaAlhemyari <sumaia.al-hemyari@hotmail.com>
2021-10-04 20:15:02 -0400

    10/4
    New Work

---
+++ test/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel=
+
+          Learn React Now!
+          <HelloWorld name = 'Sonyl'/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ test/src/HelloWorld.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+
+const HelloWorld = (props) => {
+const [counter, setCounter] = useState(0);
+
+const incrementCounter = () => {
+    setCounter(counter+1)
+}
+    return(
+        <>
+            <>
+             <p>Hello</p>
+             <h1>{props.name}</h1>
+            </>
+            <div><p>{counter}</p></div>
+            <button onClick={incrementCounter}>Increment Me</button>
+        </>
+     )
+    }
+
+export default HelloWorld;
