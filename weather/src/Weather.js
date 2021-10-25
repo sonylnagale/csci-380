0664eb8ece553a2b1faa411253b00db20f8419a1
vincepogz <85046947+vincepogz@users.noreply.github.com>
2021-10-25 19:48:57 -0400

    ruunning

--- weather/src/Weather.js
+++ weather/src/Weather.js
@@ -1,14 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import env from "react-dotenv";
 
 const { OPEN_WEATHER_MAP_API_KEY } = env
 
-const Weather = () => {
+const Weather = () => 
+{
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=40.7707237&lon=-73.9914229&appid=${OPEN_WEATHER_MAP_API_KEY}&units=imperial`)
+
+    useEffect(() => 
+    {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=40.7707237&lon=-73.9914229&appid=${env.OPEN_WEATHER_MAP_API_KEY}&units=imperial`)
+            .then(data => data.json())
+            .then(json => {
+                console.log(json)
+            })
+
+    }, [])
 
     const [weather, setWeather] = useState({})
-    const [latlon, setLatlon] = useState({
-        latitude: undefined,
-        longitude: undefined
+
+    const [latlon, setLatlon] = useState
+    ({
+            latitude: undefined,
+            longitude: undefined
     })
 
     return (
@@ -18,4 +33,4 @@ const Weather = () => {
     )
 }
 
-export default Weather
+export default Weather;
